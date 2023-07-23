ab8be49f0a3e38dccabcb202676240868702e1f0
Mostafa Moradian <mostafa@gatewayd.io>
2023-07-23 20:14:51 +0200

    Generate last_modified_date using Git history

---
+++ generate_last_modified_data.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+# Replace `last_modified_date` timestamp with current time
+# https://mademistakes.com/notes/adding-last-modified-timestamps-with-git/#mention-5
+
+for b in $(find ./ -name '*.md'); do
+    if [ $(cat ${b} | head -n 1 | grep -E '\-\-\-') ]; then
+        echo "already have meta"
+    else
+        cat ${b} | sed '1s/^/---\n---\n/' >tmp
+        mv tmp ${b}
+    fi
+
+    if grep -q last_modified_date ${b}; then
+        echo "no change"
+    else
+        cat ${b} | sed '1 {/---.*/,/---.*/{0,/---/{s/---/---\nlast_modified_date:/}}}' >tmp
+        mv tmp ${b}
+    fi
+
+    cat ${b} | sed "/---.*/,/---.*/s/^last_modified_date:.*$/last_modified_date: $(git log -1 --pretty="format:%ci" ${b})/" >tmp
+    mv tmp ${b}
+done
+
+# Skip files
+git restore .vale/write-good/README.md
+git restore README.md
